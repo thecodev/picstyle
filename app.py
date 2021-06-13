d0e00253160afa5ae49d36194c2e1e1ef8e5d55c
Mohammad Sartaj <sartajansari9786@gmail.com>
2021-06-13 21:59:58 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, jsonify, make_response
 import cv2
 from neuralStyle import loadModel, preprocess, predict
 import numpy as np 
@@ -14,7 +14,7 @@ def convertToBinaryData(filename):
 		blobData = file.read()
 	return blobData
 
-@app.route('/')
+@app.route('/', methods=['POST', "GET"])
 def index():
 	return render_template('index.html', predicted=False)
 
@@ -40,8 +40,34 @@ def show():
 	return render_template('index.html', predicted=True, img=image.decode('utf-8'))
 
 
+@app.route('/capture', methods=['POST', 'GET'])
+def capture():
+	print("ok")
+	image = request.files["image"]
+	try:
+		image = Image.open(image)
 
-# pil_image = Image.open(image)
+		image = np.array(image)
+		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+		blob = preprocess(image)
+
+		net = loadModel("starry_night.t7")
+
+		image = predict(net, blob)
+		image = cv2.normalize(image,   np.zeros((image.shape[1], image.shape[0])), 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		image = Image.fromarray(image)
+		data = io.BytesIO()
+		image.save(data, "JPEG")
+		image = base64.b64encode(data.getvalue())
+		print("done")
+		return jsonify({'status': True, 'image': image.decode('utf-8')})
+	except Exception as e:
+		print(e)
+		return jsonify({'status': False,})
+
+	
+
 
 
 if __name__ == "__main__":
