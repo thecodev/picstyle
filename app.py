9d6a69c70f6036b78d403a77b6cf42eb6722f014
Mohammad Sartaj <sartajansari9786@gmail.com>
2021-06-14 22:23:10 +0530

    Fourth Commit

--- app.py
+++ app.py
@@ -3,10 +3,18 @@ from flask import Flask, render_template, Response, request, jsonify, make_respo
 import cv2
 from neuralStyle import loadModel, preprocess, predict
 import numpy as np 
-import base64, io
-
+import base64, io, os
 app = Flask(__name__)
 
+images = os.listdir("static/images")
+models = os.listdir("models")
+
+displayModelImages = []
+for model in models:
+	for img in images:
+		if model.split(".")[0] in img:
+			displayModelImages.append(img)
+print(displayModelImages)
 
 def convertToBinaryData(filename):
 	#Convert digital data to binary format
@@ -16,33 +24,14 @@ def convertToBinaryData(filename):
 
 @app.route('/', methods=['POST', "GET"])
 def index():
-	return render_template('index.html', predicted=False)
+	return render_template('index.html', modelImages=displayModelImages)
 
-@app.route('/show', methods=['POST', "GET"])
-def show():
-	image = request.files['img']
-	image = Image.open(image)
-	image = np.array(image)
-	image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-	blob = preprocess(image)
-
-	net = loadModel("starry_night.t7")
-
-	image = predict(net, blob)
-	image = cv2.normalize(image,   np.zeros((image.shape[1], image.shape[0])), 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
-	image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-	image = Image.fromarray(image)
-	data = io.BytesIO()
-	image.save(data, "JPEG")
-	image = base64.b64encode(data.getvalue())
-
-
-	return render_template('index.html', predicted=True, img=image.decode('utf-8'))
 
 
 @app.route('/capture', methods=['POST', 'GET'])
 def capture():
 	print("ok")
+	model = request.values.get("style").split(".")[0] + ".t7"
 	image = request.files["image"]
 	try:
 		image = Image.open(image)
@@ -51,7 +40,7 @@ def capture():
 		image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 		blob = preprocess(image)
 
-		net = loadModel("starry_night.t7")
+		net = loadModel(model)
 
 		image = predict(net, blob)
 		image = cv2.normalize(image,   np.zeros((image.shape[1], image.shape[0])), 0, 255, cv2.NORM_MINMAX).astype(np.uint8)
